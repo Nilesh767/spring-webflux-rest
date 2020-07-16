7305723c6d7412b68bb4806acb8c1c1249df62ad
nilesh767 <preneon@767>
2020-07-16 15:18:37 +0530

    Implemented Create and Update Vendor.

--- src/test/java/com/n3o/springwebfluxrest/controllers/VendorControllerTest.java
+++ src/test/java/com/n3o/springwebfluxrest/controllers/VendorControllerTest.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import static com.n3o.springwebfluxrest.domain.Vendor.builder;
+import static org.mockito.ArgumentMatchers.any;
 
 class VendorControllerTest {
 
@@ -26,8 +30,8 @@ class VendorControllerTest {
     @Test
     void list() {
         BDDMockito.given(vendorRepository.findAll())
-                .willReturn(Flux.just(Vendor.builder().firstName("abc").lastName("wfbwy").build(),
-                        Vendor.builder().firstName("asd").lastName("defw").build()));
+                .willReturn(Flux.just(builder().firstName("abc").lastName("wfbwy").build(),
+                        builder().firstName("asd").lastName("defw").build()));
         webTestClient.get().uri("/api/v1/vendors")
                 .exchange()
                 .expectBodyList(Vendor.class)
@@ -37,10 +41,41 @@ class VendorControllerTest {
     @Test
     void getById() {
         BDDMockito.given(vendorRepository.findById("someId"))
-                .willReturn(Mono.just(Vendor.builder().firstName("cat1").lastName("bdvwbe").build()));
+                .willReturn(Mono.just(builder().firstName("cat1").lastName("bdvwbe").build()));
 
         webTestClient.get().uri("/api/v1/vendors/someId")
                 .exchange()
                 .expectBody(Vendor.class);
+    }
+
+    @Test
+    void testCreate() {
+        BDDMockito.given(vendorRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(builder().firstName("some name").build()));
+
+        Mono<Vendor> vendorMono = Mono.just(builder().firstName("some fname").build());
+
+        webTestClient.post()
+                .uri("/api/v1/vendors")
+                .body(vendorMono, Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+    @Test
+    void testUpdate() {
+        BDDMockito.given(vendorRepository.save(any(Vendor.class)))
+                .willReturn(Mono.just(builder().firstName("some name").build()));
+
+        Mono<Vendor> vendorMono = Mono.just(builder().firstName("some fname").build());
+
+        webTestClient.put()
+                .uri("/api/v1/vendors/someId")
+                .body(vendorMono, Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
+
     }
 }
